f9e6250e27c0f521d06e452b344b467e0200a301
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-03 09:27:26 +1000

    clean up code

--- init.py
+++ init.py
@@ -8,34 +8,6 @@ def init():
   password="c0912c8d3a270ada868bc56862354c8211086af917523e07b60bba30403c6417",
   database="tulpas"
 )
-#     #initialize/create connection to db
-#     cursor = database_obj.cursor()
-#     #initialize MainHMC table
-#     cursor.execute('''CREATE TABLE 'main_HMC' (
-# 	'id' INT NOT NULL AUTO_INCREMENT,
-# 	'h_name' TEXT,
-# 	'h_age' INT,
-# 	'email' TEXT,
-# 	'desc' TEXT,
-# 	'path' TEXT,
-# 	'v_status' BOOLEAN DEFAULT '0',
-# 	'creation_time' TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP'
-# );''')
-#     #initialize RawData table           
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS RawData (dataID INTEGER AUTO_INCREMENT PRIMARY KEY, 
-#     dataPath TEXT NOT NULL, upload_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-#     hID INTEGER, FOREIGN KEY(hID) REFERENCES MainHMC (hostID));''')
-#     #initialize Tulpas table
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS Tulpas (tulpaID INTEGER PRIMARY KEY AUTO_INCREMENT,
-#     tulpaName TEXT NOT NULL, rec_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-#     hID INTEGER, FOREIGN KEY(hID) REFERENCES MainHMC (HostID));''')
-#     #initialize assets table
-#     #type 1 = html
-#     #type 2 = cover
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS assets (ID INTEGER PRIMARY KEY AUTO_INCREMENT, 
-#     assetPath TEXT NOT NULL, type TEXT NOT NULL, 
-#     hID INTEGER, FOREIGN KEY(hID) REFERENCES MainHMC (HostID));''')
-#     #commit
-#     cursor.commit()
+
     return (database_obj)
     
--- mail.py
+++ mail.py
@@ -1,23 +1,30 @@
 import smtplib
 from email.mime.text import MIMEText
-
-class send_email:
+import ssl
+class send_email():
     def __init__(self, recp_eml):
        self.recp_eml = recp_eml
         
     def send(self):
+        #sender email
         sender = 'tulpa-hmc-no_reply@outlook.com'
+        #receiver email
         receivers = [self.recp_eml]
 
+        port = 587
+        msg = MIMEText(""+""+"")
+        #Message
+        msg['Subject'] = '您的花名册申请未通过审核'
+        msg['From'] = 'tulpa-hmc-no_reply@outlook.com'
+        msg['To'] = receivers
 
-        port = 1025
-        msg = MIMEText('This is test mail')
-
-        msg['Subject'] = 'Test mail'
-        msg['From'] = 'admin@example.com'
-        msg['To'] = 'info@example.com'
+        #create ssl context for ssl connection to SMTP
+        context = ssl.create_default_context()        
         #connect to outlook smtp and send
-        with smtplib.SMTP("smtp.office365.com", port) as server:
-            server.login(sender, "c0912c8d3a270ada868bc56862354c8211086af917523e07b60bba30403c6417")
-            server.sendmail(sender, receivers, msg.as_string())
+        with smtplib.SMTP_SSL("smtp.office365.com", port, context) as server:
+            server.ehlo() 
+            server.starttls()#start TLS connection
+            server.login(sender, "c0912c8d3a270ada868bc56862354c8211086af917523e07b60bba30403c6417")#login to SMTP
+            server.sendmail(sender, receivers, msg.as_string())#send email via SMTP
+            server.close()#close connection
             print("Successfully sent email")
