fc2b9a415c1dc0fd46144342c8711204458a6bcb
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-12 12:11:10 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
 import asyncio
-from cProfile import run
-from multiprocessing import Process
 import websockets
 from parser import parse_all
 from os import environ, path
 import signal
 import init
-import threading
-from multiprocessing import Semaphore
 #returned mem_con object from init
 memcon = init.init()
-concurrency = 3
+        
 #main handler of request
-async def handler(websocket,sema):
+async def handler(websocket):
     print("Got request")
     remote_ip = websocket.remote_address
     print("Client Disconnected with IP:", remote_ip)    
@@ -26,22 +22,9 @@ async def handler(websocket,sema):
     #Catch connection reset by peer
     except ConnectionResetError:
         print("Connection reset by peer with IP:", remote_ip)
-async def handler(websocket):
-    sema = Semaphore(concurrency)
-    sema.acquire()
-    p = Process(target=handler, args=(websocket, sema))
-    p.start()
-    p.join()
-def main():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    ws_server = websockets.serve(handler,"",2186)
-    loop.run_until_complete(ws_server)
-    loop.run_forever()
-    loop.close()
-#main function
 
-#async def main():
+#main function
+async def main():
 
     #Serves WS behind NGINX via UNIX Socket
     # loop = asyncio.get_running_loop()
@@ -51,9 +34,9 @@ def main():
     #     await asyncio.Future()
 
 
- #   async with websockets.serve(handler,"",2186):
-#      await asyncio.Future()
+    async with websockets.serve(handler,"",2186):
+        await asyncio.Future()
 #ping client
 #run main function
 
-main()
+asyncio.run(main())
