b2cb30860d71912d73862d6b71ace4558afad493
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-14 12:54:56 +1000

    Update methods.py

--- methods.py
+++ methods.py
@@ -104,18 +104,21 @@ def cover_database(c_name, query_hmc, conn_mem):
     #query for executing code for hmc cover
     Database_operation(sql_hmc_cover,conn_mem, 2, "assets").conn()
     sema.release()
+    return
 def uploading_tulpa(i, parsed_json, query_hmc, conn_mem):
     sema.acquire()
     sql_tulpa = concatenate_sql().insert_tulpa(i, parsed_json, query_hmc)
     print(sql_tulpa)
     Database_operation(sql_tulpa, conn_mem, 2, "tulpas").conn()
     sema.release()
+    return
 def uploading_webinput(f_name, query_hmc, conn_mem):
     sema.acquire()
     #concatenate sql for storing webinput records in asset table
     sql_hmc_webinput = concatenate_sql().insert_doc("webinput", f_name+".html", query_hmc)
     Database_operation(sql_hmc_webinput, conn_mem, 2, "assets").conn()
     sema.release()
+    return
 def writing_image(host_path, parsed_json, i):
     sema.acquire()
     #decoding image from base64 and write them into perspective files
@@ -123,9 +126,11 @@ def writing_image(host_path, parsed_json, i):
     image_file.write(base64.b64decode(parsed_json["imgs"][i]))
     image_file.close()
     sema.release()
+    return
 def writing_cover(host_path, parsed_json):
     sema.acquire()
     cover_file = open(os.path.join(host_path, parsed_json["cover_name"]), 'wb')
     cover_file.write(base64.b64decode(parsed_json["cover"]))
     cover_file.close()
     sema.release()
+    return
