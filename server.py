3324fdaf83bc24811c8120ba50e6bcc642c707c4
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-14 12:51:53 +1000

    Update server.py

--- server.py
+++ server.py
@@ -6,51 +6,39 @@ import signal
 import init
 from concurrent.futures import ProcessPoolExecutor
 #returned mem_con object from init
-
+memcon = init.init()
 #task executor for multiprocessing
-task_executer = ProcessPoolExecutor(max_workers=2)
+task_executer = ProcessPoolExecutor(max_workers=3)
 #main handler of request
-async def get_response(message):
-    loop = asyncio.get_running_loop()
-    return await loop.run_in_executor(task_executer, parse_all, message)
-async def sender(websocket, message):
-    remote_ip = websocket.remote_address 
+async def handler(websocket):
+    print("Got request")
+    remote_ip = websocket.remote_address
+    print("Client Disconnected with IP:", remote_ip)    
     try:
-        await websocket.send(str(message))
+        message = await websocket.recv()
+        response = parse_all(message, memcon)
+        #print(response)
+        await websocket.send(str(response))
     except websockets.ConnectionClosedOK:
         print("Client Disconnected with IP:", remote_ip)
-async def handler(websocket):
-
-    print("Got request")
- 
-    #multiprocessing
-
-    message = await websocket.recv()
-    response = await get_response(message)
-    print(response)
-    await sender(websocket, response)
-    
-        # for task in asyncio.asyncio.as_completed(tasks):
-        #     response = await task
-        #     loop.create_task(sender(websocket, response))
-
-
+    #Catch connection reset by peer
+    except ConnectionResetError:
+        print("Connection reset by peer with IP:", remote_ip)
 
 #main function
-# async def main():
+async def main():
 
-#     #Serves WS behind NGINX via UNIX Socket
-#     # loop = asyncio.get_running_loop()
-#     # stop = loop.create_future()
-#     # loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-#     # async with websockets.unix_serve(handler, path=f"{os.environ['SUPERVISOR_PROCESS_NAME']}.sock",):
-#     #     await asyncio.Future()
+    #Serves WS behind NGINX via UNIX Socket
+    # loop = asyncio.get_running_loop()
+    # stop = loop.create_future()
+    # loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    # async with websockets.unix_serve(handler, path=f"{os.environ['SUPERVISOR_PROCESS_NAME']}.sock",):
+    #     await asyncio.Future()
 
 
-
+    async with websockets.serve(handler,"",2186):
+        await asyncio.Future()
 #ping client
 #run main function
-main = websockets.serve(handler,"",2186)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main)
-loop.run_forever()
+
+asyncio.run(main())
