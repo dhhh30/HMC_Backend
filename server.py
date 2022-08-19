237df7249470861ff1842b3eb2088f9f0b547001
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-19 23:43:05 +1000

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import signal
 from parser import parse_all
 from concurrent.futures import ProcessPoolExecutor
 task_executor = ProcessPoolExecutor()
+
 async def handler(websocket):
     print("Got request")
     remote_ip = websocket.remote_address
@@ -24,8 +25,7 @@ async def handler(websocket):
         print("Connection reset by peer with IP:", remote_ip)
 #main function
 async def main():
-    async with websockets.serve(handler,"",2186):
-        await asyncio.Future()
+    websockets.serve(handler,"",2186)
 #ping client
 #run main function
 
