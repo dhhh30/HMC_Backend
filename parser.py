bebba1e8fe3cd1de6d5bf6b6bbd2d9fd21e8a4b7
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-19 23:51:25 +1000

    Update parser.py

--- parser.py
+++ parser.py
@@ -74,7 +74,6 @@ def parse_all(data):
             "sites": list_of_site
         }
         
-        conn_mem.close() 
         #serialize return_dict to json
         data = json.dumps(return_dict, indent=4)
         return (data)
@@ -142,7 +141,6 @@ def parse_all(data):
         #           "success" : "False"
         #       }
         return_json = json.dumps(return_dict, indent=4)
-        conn_mem.close()
         return (return_json)
     
     elif parsed_json['request'] == "admin":
