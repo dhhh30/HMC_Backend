f4389ed761843b78ee4c5e9e65695d1894992ecd
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-25 12:56:40 +1000

    Update methods.py

--- methods.py
+++ methods.py
@@ -91,10 +91,10 @@ class concatenate_sql:
         sql = ("""SELECT assetPath FROM assets WHERE hID='{}' AND type='{}'""".format(hID[0],type))
         return sql
     def query_approve_hmc(self, hID):
-        sql = ("""UPDATE * FROM Main_HMC WHERE hID='{}'""".format(hID))
+        sql = ("""UPDATE * FROM main_HMC WHERE hID='{}'""".format(hID))
         return (sql)
     def get_host_id(h_name):
-        sql =  """SELECT hID from Main_HMC WHERE h_name = '{}' """.format(h_name)
+        sql =  """SELECT hID from main_HMC WHERE h_name = '{}' """.format(h_name)
         return sql
     
     def query_admin_list(self, pg_num, v_status):
