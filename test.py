2080ee37b080bdc1d5b7355e5bfef594568a144e
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-28 19:10:42 +1000

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,15 @@ from crypt import methods
 import methods_ohaul
 import parser_ohaul
 import asyncio
-json = """{
-            "request":"adminAuthentication",
-            "userName":"test",
-            "password":"test"
-           }"""
+# json = """{
+#             "request":"adminAuthentication",
+#             "userName":"test",
+#             "password":"test"
+#            }"""
+json = ("""
+{"request":"mainList",
+"page": 10
+}""")
 async def main():
   print (await parser_ohaul.parse_all(json))
 asyncio.run(main())
