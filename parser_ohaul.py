cb2fb658411ef1f2661c5b85f94c70fc951b942c
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-28 13:10:36 +1000

    Update parser_ohaul.py

--- parser_ohaul.py
+++ parser_ohaul.py
@@ -9,14 +9,14 @@ async def parse_all(data):
     parsed_json = json.loads(data)
     #mainList method
     if parsed_json['request'] ==  "mainList":
-        return methods.general_request.mainList(parsed_json)
+        return methods.general_request().mainList(parsed_json)
     #handle uploading request
     elif parsed_json['request'] == "uploading":
-        return methods.general_request.uploading(parsed_json)
+        return methods.general_request().uploading(parsed_json)
     elif parsed_json['request'] == "adminAuthentaication":
-        return methods.admin_request.adminAuthentication(parsed_json)
+        return methods.admin_request().adminAuthentication(parsed_json)
     elif parsed_json['request'] == "adminList":
-        return methods.admin_request.admin_list(parsed_json)
+        return methods.admin_request().adminList(parsed_json)
     # elif parsed_json['request'] == "adminApprove":
     #     return methods.adminApprove(parsed_json)
     # elif parsed_json['request'] == "adminDeny":
