ae14304d7a24dc131987fcb079739711ee23cfc9
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-28 19:43:49 +1000

    s

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ json = ("""
 {"request":"mainList",
 "page": 100
 }""")
-async def main():
-  print (await parser_ohaul.parse_all(json))
-asyncio.run(main())
+print (type(methods_ohaul.admin.admin_authentication("test", "test")))
+print (methods_ohaul.admin.admin_authentication("test", "test"))
+# async def main():
+#   print (await parser_ohaul.parse_all(json))
+# asyncio.run(main())
