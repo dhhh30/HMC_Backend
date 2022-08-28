9cfaed5c821c5a5b899ccd17d7daf59c4caa3c7c
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-28 18:23:22 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from crypt import methods
 import methods_ohaul
-json = ("""
-        {
+json = {
             "request":"adminAuthentication",
             "userName":"test",
             "password":"test"
-           }""")
+           }
 print (methods_ohaul.admin_request.adminAuthentication(json))
