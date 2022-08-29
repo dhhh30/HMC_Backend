f000a9e7a3c80f9b8332956d8c30b0d25c93c4d5
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-29 20:35:49 +1000

    Update parser_ohaul.py

--- parser_ohaul.py
+++ parser_ohaul.py
@@ -3,7 +3,7 @@ import methods_ohaul as methods
 import json
 from concurrent.futures import ProcessPoolExecutor
 import pdb
-
+import init
 import asyncio
 # pdb.set_trace()
 def parse_all(data):
@@ -16,8 +16,29 @@ def parse_all(data):
         return methods.general_request.uploading(parsed_json)
     elif parsed_json['request'] == "adminAuthentaication":
 
-        # breakpoint()
-        return methods.adminAuthentication(parsed_json)
+        conn_mem = init.init()
+        compare_hash = methods.admin.admin_authentication(parsed_json["password"] , parsed_json["userName"])
+        # print(compare_hash, flush=True)
+        if compare_hash == True:
+            token =  methods.admin.admin_gen_token()   
+            token_sql = methods.sql_operation.token_operation(token, 1)    
+            methods.database.connect(token_sql, conn_mem, 2)
+            print(token)
+            return_dict = {
+                "request" : "adminAuthentication",
+                "Success" :  "true",
+                "token": str(token)
+            }
+            return_json = json.dumps(return_dict, indent=4)
+            return (return_json)
+        elif compare_hash == False:
+            return_dict= {
+                "request" : "adminAuthentication",
+                "Success" :  "false"
+            }
+            return_json = json.dumps(return_dict, indent=4) 
+            return (return_json)
+
     elif parsed_json['request'] == "adminList":
         return methods.admin_request.adminList(parsed_json)
     # elif parsed_json['request'] == "adminApprove":
