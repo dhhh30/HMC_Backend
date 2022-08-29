a173cc48ee3bc8cd7ba7136984e90f6af62a4eb2
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-29 23:08:41 +1000

    s

--- methods_ohaul.py
+++ methods_ohaul.py
@@ -386,6 +386,7 @@ class admin(database):
             print (token)
             creation_time = token[0][0]
             now_time = datetime.datetime.now(timezone.utc)
+            now_time = now_time.replace(tzinfo=None)
             difference = now_time - creation_time
             if int(difference.total_seconds()) >= 900:
                 database.connect(str(sql_operation.token_operation(token_i, 4)), init.init(), 2)
