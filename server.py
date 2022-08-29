6e11443f8cae2e727ca789a2202bdb387f1bfe26
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-29 18:04:14 +1000

    s

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ async def handler(websocket):
         #loop through messages sent by client
         async for message in websocket:       
             logging.debug(str(cur_datetime)+"The client at "+str(remote_ip)+"sent"+str(message))
-            response = await parse_all(message)
+            response = parse_all(message)
             logging.debug(str(cur_datetime)+"Response to client at "+str(remote_ip)+"is"+str(response))
             print(type(response))
             await websocket.send(str(response))
