69105c31be10077e5fb3082214417d15f51bd0b3
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-29 21:21:01 +1000

    Update methods_ohaul.py

--- methods_ohaul.py
+++ methods_ohaul.py
@@ -359,7 +359,7 @@ class admin(database):
     def admin_token_auth(token_i):
         token = database.connect(str(sql_operation.token_operation(token_i, 3)), init.init(), 1)
         print(token)
-        if token[0] == 0:
+        if token[0][0] == 0:
             return False
         else:
             token = database.connect(str(sql_operation.token_operation(token_i, 2)), init.init(), 1)
