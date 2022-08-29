503571a4063585cf0941ae719c20a83be6b58551
dhhh30 <44889989+dhhh30@users.noreply.github.com>
2022-08-30 02:34:50 +1000

    Update methods_ohaul.py

--- methods_ohaul.py
+++ methods_ohaul.py
@@ -440,7 +440,7 @@ class admin_request(database):
         sql = sql_operation.select_sep_host(str(parsed_json['id']))
         host = database.connect(sql, init.init(), 1)
         path_update = str(host[0][7]).replace(special_auth_pass, "")
-        file_operation.move_host(path_update)
+        file_operation.move_host(host[0][7],path_update)
         sql = sql_operation.query_approve_hmc(str(parsed_json['id']), path_update)
         database.connect(sql, init.init(), 2)
         return_json = {
